14d6a0cd3d53a109387534764105f4d2be8cf61f
piedra-de-flor <xxspdlqjxx@naver.com>
2024-05-20 14:09:15 +0900

    feat : 응답값 이미지 제외 및 사람 수만 응답하도록 변경

--- app.py
+++ app.py
@@ -7,33 +7,40 @@ app = Flask(__name__)
 def detect_objects(image_files):
     model_path = 'C:/Users/USER/Desktop/공부/server_connect/best.onnx'
     model = YOLO(model_path)
-    results_list = []
+    results_data = []
+
+    person_class_id = None
+    for key, value in model.names.items():
+        if value == 'person':
+            person_class_id = key
+            break
+
+    if (person_class_id is None):
+        raise ValueError("Person class not found in the model's class names.")
 
     for filename in image_files:
         results = model(filename, project="server_connect", name="output")
 
-        for i, res in enumerate(results):
-            result_image_path = os.path.join('C:/Users/USER/Desktop/공부/server_connect', f'result_{i}.jpg')
-            res.save(result_image_path)
-
-        image_boxes = []
         for res in results:
-            boxes = res.boxes.xyxy.cpu().numpy().tolist()
-            image_boxes.extend(boxes)
-        results_list.append(image_boxes)
+            person_count = sum(1 for box in res.boxes if box.cls == person_class_id)
+            results_data.append({
+                'detected_persons': person_count
+            })
 
-    return results_list
+    return results_data
 
 @app.route('/congestion', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
-        return 'No file part'
+        return jsonify({'error': 'No file part'}), 400
     file = request.files['file']
     if file.filename == '':
-        return 'No selected file'
+        return jsonify({'error': 'No selected file'}), 400
     if file:
-        results = detect_objects([file.filename])
-        return jsonify({'result': results})
+        filename = os.path.join('C:/Users/USER/Desktop/공부/server_connect', file.filename)
+        file.save(filename)
+        results = detect_objects([filename])
+        return jsonify({'results': results})
 
 if __name__ == '__main__':
     app.run(debug=True)
